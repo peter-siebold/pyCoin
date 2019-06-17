c3764d4d286b315616daf462bcbd0e4af4e5bad6
Peter S <peter.siebold@icloud.com>
2019-06-17 16:46:37 +0200

    add routes for adding transactions

--- mxcoin.py
+++ mxcoin.py
@@ -107,3 +107,73 @@ class Blockchain:
             self.chain = longest_chain
             return True
         return False
+
+
+# Create the web app
+app = Flask(__name__)
+
+# create an address for the nod on port 5000
+node_adress = str(uuid4()).replace('-', '')
+
+# Initialize a new block chain
+blockchain= Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block["proof"]
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender = node_adress, receiver = 'MxClsy', amount = 1)
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {
+        'message' : 'You just mined a block',
+        'index' : block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+        'transactions': block['transactions']
+    }
+
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {
+        'chain' : blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+@app.route('/is_valid', methods=['GET'])
+def is_valid(): 
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid == True:
+        response = {
+        'message' : "Blockchain is valid :)"
+        }
+    else:
+        response = {
+            'message' : "Blockchain has become invalid :("
+        }
+
+    return jsonify(response), 200
+
+
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    
+    if not all (key in json for key in transaction_keys):
+        return 'Some elements of the tranactions are missing', 400
+    
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = { 'message' : f'This transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+
+app.run(host = '0.0.0.0', port = 5000)
